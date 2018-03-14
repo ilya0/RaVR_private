b20e3a79fdf7b070e26deefb97992ad51933f457
theCydonian <harris_luke@icloud.com>
2018-03-13 17:10:23 -0700

    added cube with mouse input for samples

---
+++ RaVR-Unity/Assets/Scripts/CubeButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeButton : MonoBehaviour {
+
+    public int buttonNum; //sets which button this is
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        int set = 0;
+        if (buttonNum + 1 > player.GetComponent<AudioManager>().oneDClip.Length / 2)
+            set = 1;
+
+        StartCoroutine(player.GetComponent<AudioManager>().setClip(set,(int)(buttonNum/2)));
+    }
+}
