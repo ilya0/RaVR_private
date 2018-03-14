ffe90a9e505814e00a27e73157996f9789dbd73d
theCydonian <harris_luke@icloud.com>
2018-03-13 21:56:49 -0700

    finished basic emmiter

---
+++ RaVR-Unity/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject[] UserList;
+    private float speed = 2;
+    private float mod = 0.01F;
+    float mover;
+    float timer = 5;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        mover = Random.value * 1 - 0.5F;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.Rotate(new Vector3(2,1,1));
+        transform.SetPositionAndRotation(new Vector3(transform.position.x + mover*0.1F, transform.position.y, transform.position.z+step), transform.rotation);
+    }
+}
